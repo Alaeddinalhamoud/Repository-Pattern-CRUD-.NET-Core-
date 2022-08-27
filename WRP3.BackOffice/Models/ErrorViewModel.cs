137d434154eaa8da3e68a68892081a49e0976630
Alaeddin Alhamoud <eng.aladinhamod@gmail.com>
2022-08-27 17:48:17 +0100

    BackOffice init

---
+++ WRP3.BackOffice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WRP3.BackOffice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
