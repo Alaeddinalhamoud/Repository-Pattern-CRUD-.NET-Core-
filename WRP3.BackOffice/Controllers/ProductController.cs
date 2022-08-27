49f453b260e75b3a71bf5d5e0180a41d294fa501
Alaeddin Alhamoud <eng.aladinhamod@gmail.com>
2022-08-27 18:53:32 +0100

    dash layout

---
+++ WRP3.BackOffice/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WRP3.BackOffice.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WRP3.BackOffice/Controllers/TestTypeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WRP3.BackOffice.Controllers
+{
+    public class TestTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
