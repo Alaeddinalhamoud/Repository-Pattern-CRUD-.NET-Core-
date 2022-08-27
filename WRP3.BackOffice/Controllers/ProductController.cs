1df6377f85045404d2c0049e170be368d4fcfcb8
Alaeddin Alhamoud <eng.aladinhamod@gmail.com>
2022-08-27 21:28:33 +0100

    Product

--- WRP3.BackOffice/Controllers/ProductController.cs
+++ WRP3.BackOffice/Controllers/ProductController.cs
@@ -1,12 +1,60 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WRP3.Domain.Entities;
 
 namespace WRP3.BackOffice.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ILogger<ProductController> _logger;
+
+        [TempData]
+        public string StatusMessage { get; set; }
+        public ProductController(IHttpClientFactory httpClientFactory, ILogger<ProductController> logger)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
+            _logger = logger;
+
+        }
+
+        public async Task<IActionResult> Index()
+
+        {
+            List<Product> products = new();
+            try
+            {
+                var httpRequestMessage = new HttpRequestMessage
+                           (HttpMethod.Get, "/api/product");
+
+                var httpClient = _httpClientFactory.CreateClient("API");
+
+                var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                    using var contentStream =
+                        await httpResponseMessage.Content.ReadAsStreamAsync();
+
+                    products = await JsonSerializer.DeserializeAsync
+                        <List<Product>>(contentStream);
+                    StatusMessage = "Message";
+                    return View(products);
+                }
+                return View(products = null);
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex, $"Error will processing {nameof(ProductController)}");
+                return View(products = null);
+            }
         }
     }
 }
--- WRP3.BackOffice/Startup.cs
+++ WRP3.BackOffice/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 namespace WRP3.BackOffice
 {
@@ -20,6 +21,11 @@ namespace WRP3.BackOffice
         {
             services.AddControllersWithViews()
                 .AddRazorRuntimeCompilation();
+
+            services.AddHttpClient("API", httpClient =>
+            {
+                httpClient.BaseAddress = new Uri("https://localhost:44365");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
