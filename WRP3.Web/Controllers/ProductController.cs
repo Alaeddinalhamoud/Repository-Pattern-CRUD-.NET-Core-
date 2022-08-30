f3436637704de0555894be3c805fcc183d3bc426
Alaeddin Alhamoud <eng.aladinhamod@gmail.com>
2022-08-30 22:00:02 +0100

    Review

--- WRP3.Web/Controllers/ProductController.cs
+++ WRP3.Web/Controllers/ProductController.cs
@@ -6,6 +6,8 @@ using WRP3.Domain.Entities;
 using WRP3.Infrastructure.APIServices.IServices;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
+using WRP3.Web.Models.ProductViewModels;
+using System.Collections.Generic;
 
 namespace WRP3.Web.Controllers
 {
@@ -13,16 +15,25 @@ namespace WRP3.Web.Controllers
     public class ProductController : Controller
     {
         private readonly ILogger<ProductController> _logger;
-        private readonly IAPIService<Product> _apiService;
-        const string API_URL = "/api/product";
+        private readonly IAPIService<Product> _productService;
+        private readonly IAPIService<TestType> _testTypeService;
+        private readonly IAPIService<ProductTest> _productTestService;
+
+        const string PRODUCT_API_URL = "/api/product";
+        const string PRODUCT_TEST_API_URL = "/api/ProductTest";
+        const string TEST_TYPE_API_URL = "/api/TestType";
 
         [TempData]
         public string StatusMessage { get; set; }
         public ProductController(ILogger<ProductController> logger,
-            IAPIService<Product> apiService)
+            IAPIService<Product> productService,
+            IAPIService<TestType> testTypeService,
+            IAPIService<ProductTest> productTestService)
         {
             _logger = logger;
-            _apiService = apiService;
+            _productService = productService;
+            _testTypeService = testTypeService;
+            _productTestService = productTestService;
 
         }
         public async Task<IActionResult> Index(string sortOrder, string searchString)
@@ -33,7 +44,7 @@ namespace WRP3.Web.Controllers
                 ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
                 ViewData["CurrentFilter"] = searchString;
 
-                var products = await _apiService.GetAll(API_URL);
+                var products = await _productService.GetAll(PRODUCT_API_URL);
 
                 if (!string.IsNullOrEmpty(searchString))
                     products = products.Where(s => s.Name.Contains(searchString)).ToList();
@@ -58,11 +69,44 @@ namespace WRP3.Web.Controllers
             }
             catch (Exception ex)
             {
-                StatusMessage = $"Error whilte trying to get all {typeof(ProductController)}";
+                StatusMessage = $"Error whilte trying to get all {nameof(ProductController)}";
                 _logger.LogError(ex, StatusMessage);
 
                 return RedirectToAction("Error", "Home");
             }
+        }
+
+        public async Task<IActionResult> Review(int? Id)
+        {
+            try
+           {
+                if (Id == 0 || Id is null)
+                {
+                    StatusMessage = $"Error Empty data passed {nameof(ProductController)}";
+                    return RedirectToAction("Error", "Home");
+                }
+                Product? product = await _productService.Get(Id, $"{PRODUCT_API_URL}/GetById");
+                List<TestType>? testTypes = await _testTypeService.GetAll(TEST_TYPE_API_URL);
+                List<ProductTest>? productTests = await _productTestService.GetAll(PRODUCT_TEST_API_URL);
+
+                var reviewModel = new Review()
+                {
+                    ProductId = product?.Id,
+                    ProductName = product?.Name,
+                    TestTypes = testTypes,
+                    ProductTests = productTests.Where(x => x.Product?.Id.Equals(product?.Id) ?? false).ToList()
+                };
+
+                return View(reviewModel);
+            }
+            catch (Exception ex)
+            {
+                StatusMessage = $"Error whilte trying to get all product review {nameof(ProductController)}";
+                _logger.LogError(ex, StatusMessage);
+
+                return RedirectToAction("Error", "Home");
+            }
+
         }
     }
 }
---
+++ WRP3.Web/Models/ProductViewModels/Review.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using WRP3.Domain.Entities;
+
+namespace WRP3.Web.Models.ProductViewModels
+{
+    public class Review
+    {
+        public int? ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public List<TestType>? TestTypes { get; set; }
+        public List<ProductTest>? ProductTests { get; set; }
+    }
+}
