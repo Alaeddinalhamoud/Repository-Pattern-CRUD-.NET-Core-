03d01276a040feaa9824049ba48f46a5a178500a
Alaeddin Alhamoud <eng.aladinhamod@gmail.com>
2022-08-31 21:03:22 +0100

    Chart

---
+++ WRP3.BackOffice/Models/ViewComponentModels/BarChartTestType.cs
@@ -0,0 +1,8 @@
+﻿namespace WRP3.BackOffice.Models.ViewComponentModels
+{
+    public class BarChartTestType
+    {
+        public string TestNames { get; set; }
+        public string NumberOfTest { get; set; }
+    }
+}
---
+++ WRP3.BackOffice/ViewComponents/BarChartTestTypeViewComponent.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System;
+using WRP3.Domain.Entities;
+using WRP3.Infrastructure.APIServices.IServices;
+using WRP3.BackOffice.Models.ViewComponentModels;
+using System.Linq;
+
+namespace WRP3.BackOffice.ViewComponents
+{
+    [ViewComponent]
+    public class BarChartTestTypeViewComponent : ViewComponent
+    {
+        private readonly ILogger<BarChartTestTypeViewComponent> _logger;
+        private readonly IAPIService<ProductTest> _productTestService;
+        private readonly IAPIService<TestType> _testTypeService;
+
+        const string PRODUCT_TEST_API_URL = "/api/ProductTest";
+        const string TEST_Type_API_URL = "/api/testtype";
+
+        public BarChartTestTypeViewComponent(ILogger<BarChartTestTypeViewComponent> logger,
+                                             IAPIService<ProductTest> ProductTestService,
+                                             IAPIService<TestType> testTypeService)
+        {
+            _logger = logger;
+            _productTestService = ProductTestService;
+            _testTypeService = testTypeService;
+
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            BarChartTestType barChartTestType = new BarChartTestType();
+            try
+            {
+
+                var productTests = await _productTestService.GetAll(PRODUCT_TEST_API_URL);
+
+                var testTypes = await _testTypeService.GetAll(TEST_Type_API_URL);
+
+                int counter = 1;
+                barChartTestType.TestNames = "[";
+                barChartTestType.NumberOfTest = "[";
+                foreach (var testType in testTypes)
+                {
+                    barChartTestType.TestNames += $"\"{testType.Name}\"";
+
+                    barChartTestType.NumberOfTest += $"{productTests.Where(x => x.TestTypeId == testType.Id).Count()}";
+
+
+                    if (counter != testTypes.Count())
+                    {
+                        barChartTestType.TestNames += ", ";
+                        barChartTestType.NumberOfTest += ", ";
+                    }
+
+                    counter++;
+                }
+
+                barChartTestType.TestNames += "]";
+                barChartTestType.NumberOfTest += "]";
+
+
+                return View(barChartTestType);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error while processing {nameof(BarChartTestTypeViewComponent)}");
+
+                return View(barChartTestType);
+            }
+        }
+    }
+}
